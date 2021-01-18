06abe362ebea99c97746110a5aa3b009256f6092
Vampir007one <kavani4@yandex.ru>
2021-01-18 14:12:05 +0400

    nav-menu commit

--- index.php
+++ index.php
@@ -28,31 +28,7 @@
             <img src="img/logo.png" alt="Парикмахерская Самая-Самая">
         </div>
         <nav class="nav-menu">
-            <ul>
-                <li class="active"><a href="#">Главная</a></li>
-                <li><a href="#">Салоны</a></li>
-                <li><a href="#">Цены</a></li>
-                <li><a href="#">Достижения</a>
-                    <ul>
-                        <li><a href="#">Достижения предприятия</a></li>
-                        <li><a href="#">Достижения мастеров</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Мастера</a></li>
-                <li><a href="#">Услуги</a></li>
-                <li><a href="#">Статьи</a></li>
-                <li><a href="#">О компании</a>
-                    <ul>
-                        <li><a href="#">Подробно о работе сайта</a></li>
-                        <li><a href="#">Работа и вакансии</a></li>
-                    </ul>
-                </li>
-                <li class="recall"><a href="#">Оставить отзыв</a>
-                    <ul>
-                        <li><img src="img/otziv_big.jpg"><a href="#"><img src="img/otziv_link.png"></a></li>
-                    </ul>
-                </li>
-            </ul>
+            <?php include($_SERVER['DOCUMENT_ROOT'] . "/pages/content/nav-menu.php")?>
         </nav>
     </div>
     <div class="container">
